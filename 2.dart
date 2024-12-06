23e4930d440a89ef536b9ea845d37635d3bd6962
axah710 <ahmed36085361@gmail.com>
2024-12-06 19:25:13 +0200

    Add function to reverse a string and demonstrate usage

--- 2.dart
+++ 2.dart
@@ -1,1 +1,10 @@
 //! Create a function that takes string and returns reversed string
+//! ahmed => demha
+
+void main() {
+  //? Function Declaration
+  String reverseString(String stringToReverse) {
+    return stringToReverse.split('').reversed.join();
+  }
+  print(reverseString('ahmed'));
+}
